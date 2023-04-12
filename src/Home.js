4caa64fd67008960365639e9a0e596825218a2a7
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-04-12 22:14:46 +0100

    Login button now appears always

--- src/Home.js
+++ src/Home.js
@@ -38,7 +38,7 @@ export default function Home() {
               </Link>
 
               {/* LOGIN */}
-              <Link to={"/login"} className="hidden md:flex">
+              <Link to={"/login"} className="flex">
                 <button
                   type="button"
                   className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
